c498bd0a337a2a6fdc8bae2fe1a2ec2c4bc96392
Victor Lobato <victorsilva123190@gmail.com>
2022-02-23 17:01:26 -0300

    Exemplo de Funcao de Alta Ordem + outras implementações

--- src/main/java/aula2/FuncaoAltaOrdem.java
+++ src/main/java/aula2/FuncaoAltaOrdem.java
@@ -2,8 +2,15 @@ package aula2;
 
 public class FuncaoAltaOrdem {
     public static void main(String[] args) {
-        Calculo soma = (a, b) -> a + b;
-        System.out.println(executarOperacao(soma, 1, 3));
+        Calculo SOMA = (a, b) -> a + b;
+        Calculo SUBTRACAO = (a, b) -> a - b;
+        Calculo DIVISAO = (a, b) -> a / b;
+        Calculo MULTIPLICACAO = (a, b) -> a * b;
+
+        System.out.println(executarOperacao(SOMA, 4, 2));
+        System.out.println(executarOperacao(SUBTRACAO, 4, 2));
+        System.out.println(executarOperacao(DIVISAO, 4, 2));
+        System.out.println(executarOperacao(MULTIPLICACAO, 4, 2));
     }
 
     public static int executarOperacao(Calculo calculo, int a, int b){
